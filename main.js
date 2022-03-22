fb00a9cd83404e3ee4fd3f338a107097f7a8189f
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-03-22 15:21:07 +0100

    js terminato

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 let chilometriDaPercorrere=prompt("inserire chilometri da percorrere");
 chilometriDaPercorrere=parseInt(chilometriDaPercorrere);
+
 let etaPasseggero=prompt("inserire eta' passeggero");
 etaPasseggero=parseInt(etaPasseggero);
+
 let prezzoPerBiglietto=chilometriDaPercorrere*0.21;
-let prezzoFinale;
+let prezzoFinal;
 let sconto;
+let prezzoFinale;
+let euro="€";
+let chilometri="km";
+let anni="anni";
+
 if(etaPasseggero<18){
-    sconto=prezzoPerBiglietto*0.20;
-    prezzoFinale=prezzoPerBiglietto-sconto;
-    alert(prezzoFinale.toFixed(2));
+    sconto=prezzoPerBiglietto*0.21;
+    prezzoFinaleIntero=prezzoPerBiglietto-sconto;
+    prezzoFinale=prezzoFinaleIntero.toFixed(2) + euro;
 }else if(etaPasseggero>65){
     sconto=prezzoPerBiglietto*0.40;
-    prezzoFinale=prezzoPerBiglietto-sconto;
-    alert(prezzoFinale.toFixed(2));
+    prezzoFinaleIntero=prezzoPerBiglietto-sconto;
+    prezzoFinale=prezzoFinaleIntero.toFixed(2) + euro;
 }else{
-    sconto=prezzoPerBiglietto;
-    alert(prezzoFinale.toFixed(2));
+    prezzoFinale=prezzoPerBiglietto.toFixed(2) + euro;
 }
-
-//etaPasseggero>=18)||(etaPasseggero<=65
+document.getElementById("chilometriDaPercorrere").innerHTML= chilometriDaPercorrere;
+document.getElementById("etaPasseggero").innerHTML= etaPasseggero;
+document.getElementById("prezzoFinale").innerHTML= prezzoFinale;
